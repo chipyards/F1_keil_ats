5b30c0c3230292bce0329b8bf9117ff9ae210f29
J.L. Noullet <noullet@chipyards.com>
2023-03-16 12:43:13 +0100

    WAV2SRC: refonte du prog de compression (MinGW32), supporte varier frequ. audio et oversampling sur multiples profils

--- WAV2SRC/appli.c
+++ WAV2SRC/appli.c
@@ -13,16 +13,13 @@
 #include "wav_head.h"
 #include "codec.h"
 
-
 void usage()
 {
 fprintf( stderr,
-"\nUsage : wav2src source.wav dest.s -a   # asm pour Keil (style CHTI)"
-"\n        wav2src source.wav dest.c -c   # C non comprime compatible CHTI"
-"\n        wav2src source.wav dest.c -n   # C non comprime , struct nommee"
-"\n        wav2src source.wav dest.wav -w # test compression"
-"\n        wav2src source.wav dest.c -z   # C comprime, 11025 Hz"
-"\n        wav2src -       dequant.c -t   # table de dequantification en C , 11025 Hz\n\n");
+"\nUsage : wav2src P source.wav dest.wav -w # test compression-decompression"
+"\n        wav2src P source.wav dest.c -z   # C comprime"
+"\n        wav2src P -       dequant.c -t   # table de dequantification en C"
+"\navec P = indice du profil de codage\n\n");
 exit(1);
 }
 
@@ -95,72 +92,6 @@ close( s->hand );
 // printf("lu %d samples Ok\n", totalframes );
 }
 
-// Keil style ASM source code style CHTI
-void write_keil_asm( FILE * dfil, wavpars * s, float * mbuf )
-{
-fprintf( dfil, "\tAREA DonneeSon, DATA, READONLY\n");
-fprintf( dfil, "\texport LongueurSon\n");
-fprintf( dfil, "\texport PeriodeSonMicroSec\n");
-fprintf( dfil, "\texport Son\n");
-
-fprintf( dfil, "LongueurSon\tDCD\t%u\n", s->wavsize );
-fprintf( dfil, "PeriodeSonMicroSec\tDCD\t%d\n", (int)( round( 1000000.0/(double)s->freq ) ) );
-
-fprintf( dfil, "Son "); 
-// on a le son entier en mono dans mbuf[], convertissons le en ASM "Keil"
-for	( int i = 0; i < s->wavsize; ++i )
-	{
-	fprintf( dfil, "\tdcw\t%d\n", (int)floor(32768.0*mbuf[i]) );
-	}
-fprintf( dfil, "\tend\n"); 
-}
-
-// C source code style CHTI
-void write_sl16_c( FILE * dfil, wavpars * s, float * mbuf )
-{
-fprintf( dfil, "const unsigned int LongueurSon = %u;\n", s->wavsize );
-fprintf( dfil, "const unsigned int PeriodeSonMicroSec = %d;\n", (int)( round( 1000000.0/(double)s->freq ) ) );
-
-fprintf( dfil, "const short Son[] = {\n"); 
-// on a le son entier en mono dans mbuf[], convertissons le en C
-int i;
-for	( i = 0; i < ( s->wavsize - 1 ); ++i )
-	{
-	fprintf( dfil, "%d, ", (int)floor(32768.0*mbuf[i]) );
-	if	( ( i % 8 ) == 7 )
-		fprintf( dfil, "\n" );
-	}
-// dernier element sans la virgule
-fprintf( dfil, "%d };\n", (int)floor(32768.0*mbuf[i]) );
-}
-
-// C source code avec struct nommee pour multi-son
-void write_sl16_nom_c( FILE * dfil, wavpars * s, float * mbuf, char * nom )
-{
-
-fprintf( dfil, "#include \"../audio.h\"\n" );
-fprintf( dfil, "#ifdef USE_%s\n", nom );
-fprintf( dfil, "/* pour utiliser ce son, declarer :\n" );
-fprintf( dfil, "#define USE_%s\n", nom );
-fprintf( dfil, "extern const type_son %s;\n", nom );
-fprintf( dfil, "*/\n" );
-fprintf( dfil, "static const short leson[] = {\n"); 
-// on a le son entier en mono dans mbuf[], convertissons le en C
-int i;
-for	( i = 0; i < ( s->wavsize - 1 ); ++i )
-	{
-	fprintf( dfil, "%d, ", (int)floor(32768.0*mbuf[i]) );
-	if	( ( i % 8 ) == 7 )
-		fprintf( dfil, "\n" );
-	}
-// dernier element sans la virgule
-fprintf( dfil, "%d };\n", (int)floor(32768.0*mbuf[i]) );
-// la structure, enfin
-fprintf( dfil, "const type_son %s = { .longson=%u, .periodus=%u, .son=leson };	// style C99\n",
-		nom, s->wavsize, (int)( round( 1000000.0/(double)s->freq ) ) );
-fprintf( dfil, "#endif\n");
-}
-
 // production WAV file pour test auditif compression-decompression
 // le fichier source est deja lu dans mbuf
 // le fichier dest est a creer
@@ -177,8 +108,8 @@ if	( retval <= 0 )
 	gasp("echec compress2w32 : %d", retval );
 unsigned int qw32 = (unsigned int)retval;
 printf("compression effectuee : %u words de 32 bits\n", qw32 );
-unsigned int qbytes = ((s->wavsize*QBIT)/8)+1;
-printf("codage %d bits soit %u bytes = %g kbytes\n", QBIT, qbytes, ((double)qbytes)/1024.0 );
+unsigned int qbytes = ((s->wavsize*P->QBIT)/8)+1;
+printf("codage %d bits soit %u bytes = %g kbytes\n", P->QBIT, qbytes, ((double)qbytes)/1024.0 );
 // depacker et decomprimer l'audio fourni dans wbuf (unsigned int)
 // alloue la memoire pour mbuf (float normalise -1.0;1.0)
 float * mbuf2;
@@ -228,8 +159,8 @@ if	( retval <= 0 )
 	gasp("echec compress2w32 : %d", retval );
 unsigned int qw32 = (unsigned int)retval;
 printf("compression effectuee : %u words de 32 bits\n", qw32 );
-unsigned int qbytes = ((s->wavsize*QBIT)/8)+1;
-printf("codage %d bits soit %u bytes = %g kbytes\n", QBIT, qbytes, ((double)qbytes)/1024.0 );
+unsigned int qbytes = ((s->wavsize*P->QBIT)/8)+1;
+printf("codage %d bits soit %u bytes = %g kbytes\n", P->QBIT, qbytes, ((double)qbytes)/1024.0 );
 // convertissons le binaire 32 bits en texte C
 fprintf( dfil, "%u,   // le nombre d'echantillons\n", qsamp );
 int i;
@@ -246,22 +177,25 @@ fprintf( dfil, "#endif\n");
 
 void write_comp_table( FILE * dfil )
 {
-codec_init();
 codec_dump( dfil );
 }
 
 int main( int argc, char ** argv )
 {
 wavpars s;
+int ip;		// indice profil
 char snam[256];
 char dnam[256];
 FILE * dfil;
 float * mbuf;
 
-if ( argc != 4 ) usage();
+if ( argc != 5 ) usage();
 
-sprintf( snam, "%s", argv[1] );
-sprintf( dnam, "%s", argv[2] );
+ip = atoi(argv[1]);
+codec_init( ip );
+
+sprintf( snam, "%s", argv[2] );
+sprintf( dnam, "%s", argv[3] );
 
 if	( snam[0] != '-' )
 	{
@@ -272,11 +206,12 @@ if	( snam[0] != '-' )
 	wave_read_body_mono( &s, &mbuf );
 	close( s.hand );
 	printf("%u ech. @ %u Hz, duree %g s\n", s.wavsize, s.freq, (double)s.wavsize / (double)s.freq );
-	if	( s.freq != NOM_FSAMP )
-		printf("ATTENTION : freq d'echantillonnage %u au lieu de %u\n", s.freq, NOM_FSAMP );
+	unsigned int nom_fsamp = ( P->FCK / P->PWMdivider ) / P->Koversamp;
+	if	( s.freq != nom_fsamp )
+		printf("NOTE : freq d'echantillonnage %u au lieu de %u\n", s.freq, nom_fsamp );
 	}
 
-if	( argv[3][1] != 'w' )
+if	( argv[4][1] != 'w' )
 	{
 	printf("ouverture %s en ecriture\n", dnam );
 	dfil = fopen( dnam, "w" );
@@ -285,17 +220,8 @@ if	( argv[3][1] != 'w' )
 else	dfil = NULL;
 
 // ici on choisit le format de sortie
-switch	( argv[3][1] )
+switch	( argv[4][1] )
 	{
-	case 'a' : write_keil_asm( dfil, &s, mbuf ); break;
-	case 'c' : write_sl16_c( dfil, &s, mbuf ); break;
-	case 'n' :
-		{	// extraire le prenom du son
-		for	( int i = 1; i < strlen(dnam); ++i )
-			if	( dnam[i] == '.' )
-				dnam[i] = 0;
-		write_sl16_nom_c( dfil, &s, mbuf, dnam );
-		} break;
 	case 'w' : write_codec_wav( &s, mbuf, dnam ); break;
 	case 'z' :
 		{	// extraire le prenom du son
--- WAV2SRC/codec.c
+++ WAV2SRC/codec.c
@@ -4,17 +4,48 @@
 #include <stdlib.h>
 #include "codec.h"
 
+// profils de codage
+profile_t profils[] = {
+	{		// original CHTI 11k oversamp 2
+	72000000,	// FCK
+	3266,		// PWM range reso 11.7 bits
+	2,		// oversamp
+	5,		// nombre de bits du code
+	4,		// nombre de valeurs de la partie lineaire du code
+	0.4,		// (max mag) / (PWMdiv/2) = coeff de limitation de la mag du code
+	0, 0, 0, 0, 0
+	},
+	{		// 22k sans oversamp (PWM 22k)
+	72000000,	// FCK
+	3266,		// PWM range reso 11.7 bits
+	1,		// oversamp
+	5,		// nombre de bits du code
+	4,		// nombre de valeurs de la partie lineaire du code
+	0.4,		// (max mag) / (PWMdiv/2) = coeff de limitation de la mag du code
+	0, 0, 0, 0, 0
+	},
+	{		// 22k oversamp 2 (PWM 44k)
+	72000000,	// FCK
+	1633,		// PWM range reso 10.7 bits
+	2,		// oversamp
+	5,		// nombre de bits du code
+	4,		// nombre de valeurs de la partie lineaire du code
+	0.4,		// (max mag) / (PWMdiv/2) = coeff de limitation de la mag du code
+	0, 0, 0, 0, 0
+	}
+};
 
-// parametres derives
+profile_t * P = NULL;
 
+#define QMAX 256	// QBIT <= 8 ==> (1<<QBIT) <= 256 ==> QCODE < 256 ==> QLOG < 256
 // les limites de plages cote diff source, t.q. ( bnd[n-1] <= x < bnd[n] ) ==> y = QLIN + n  
 // cas limite inferieur				( QLIN-0.5 <= x < bnd[0] ) ==> y = QLIN
 // cas limite superieur : bnd[QLOG-1] n'est pas applique vu qu'il n'y a rien apres
-float bnd[QLOG-1];
+float bnd[QMAX];		// float bnd[QLOG-1];
 // les valeurs de restitution, qui tombent "au milieu" des plages, t.q. y --> ddi[y-QLIN]
-int ddi[QLOG];
+int ddi[QMAX];			// int ddi[QLOG];
 // la table de dequantification
-int dequant[1<<QBIT];
+int dequant[QMAX];		// int dequant[1<<QBIT];
 
 // tmp static storage
 int oldsig_d;	// predicteur du decodeur
@@ -24,18 +55,28 @@ float oldsig_e;	// predicteur de l'encodeur
 unsigned short quant( float diff );
 
 // initialisation
-void codec_init()
+void codec_init( int ip )
 {
 oldsig_d = 0;
 oldsig_e = 0.0;
+// profil
+P = &profils[ip];
+// parametres derives
+if	( P->QBIT > 8 )
+	printf("WARNING P->QBIT > 8 unsupported\n");
+P->target_pk = P->PWMdivider / 2;	// amplitude peak sur target
+P->QCODE     = 1 << (P->QBIT-1);	// nombre de valeurs de la magnitude du code
+P->QLOG      = P->QCODE - P->QLIN;	// nombre de bits de la partie "log" du code
+P->codemagmask = P->QCODE-1;		// mask pour la magnitude du code
+P->codesgnmask = P->QCODE;		// mask pour le bit de signe du code
 // calcul des plages en progression exponentielle soit une serie constituee de :
 // QLIN-0.5, ddi[0], bnd[0], ddi[1], bnd[1], ... ddi[QLOG-1] soit 2*QLOG elements,
 // ddi[QLOG-1] etant le plafond, determine par kplaf.
 // le facteur de progression est donc :
-double kexp = pow( ( ((double)target_p) * kplaf ) / ((double)(QLIN-0.5)), 1.0 / ((double)((2*QLOG)-1)) );
-double _ddi, _bnd = ((double)(QLIN-0.5));
+double kexp = pow( ( ((double)P->target_pk) * P->kplaf ) / ((double)(P->QLIN-0.5)), 1.0 / ((double)((2*P->QLOG)-1)) );
+double _ddi, _bnd = ((double)(P->QLIN-0.5));
 int i;
-for	( i = 0; i < ( QLOG - 1 ); ++i )
+for	( i = 0; i < ( P->QLOG - 1 ); ++i )
 	{
 	_ddi = _bnd * kexp;
 	_bnd = _ddi * kexp;
@@ -43,15 +84,15 @@ for	( i = 0; i < ( QLOG - 1 ); ++i )
 	bnd[i] = _bnd;
 	}
 _ddi = _bnd * kexp;
-ddi[QLOG-1] = (int)round(_ddi);
+ddi[P->QLOG-1] = (int)round(_ddi);
 // table de dequantification
 unsigned int mag, sgn, code;
-for	( code = 0; code < ( 1 << QBIT ); ++code )
+for	( code = 0; code < ( 1 << P->QBIT ); ++code )
 	{
-	mag = code & codemagmask;
-	sgn = code & codesgnmask;
-	if	( mag >= QLIN )
-		mag = ddi[mag-QLIN];
+	mag = code & P->codemagmask;
+	sgn = code & P->codesgnmask;
+	if	( mag >= P->QLIN )
+		mag = ddi[mag-P->QLIN];
 	dequant[code] = (sgn?(-((int)mag)):((int)mag));
 	}
 }
@@ -59,17 +100,21 @@ for	( code = 0; code < ( 1 << QBIT ); ++code )
 void codec_dump( FILE * dfil )
 {
 fprintf( dfil, "/* compression predictive avec perte\n" );
-fprintf( dfil, "amplitude nominale target_p = %u (soit (Fck/fsamp)/4 )\n", target_p );
-fprintf( dfil, "kplaf = %g, %u bits de code soit QLIN = %u, QLOG = %u\n", kplaf, QBIT, QLIN, QLOG );
+fprintf( dfil, "PWM resolution = %d\n", P->PWMdivider );
+fprintf( dfil, "amplitude nominale target_pk = %d\n", P->target_pk );
+fprintf( dfil, "frequence horloge = %d Hz\n", P->FCK );
+fprintf( dfil, "frequence PWM = %g Hz\n", (double)P->FCK / (double)P->PWMdivider );
+fprintf( dfil, "frequence audio = %g Hz\n", ( (double)P->FCK / (double)P->PWMdivider ) / (double)P->Koversamp );
+fprintf( dfil, "kplaf = %g, %u bits de code soit QLIN = %u, QLOG = %u\n", P->kplaf, P->QBIT, P->QLIN, P->QLOG );
 fprintf( dfil, "serie exponentielle :\n");
-fprintf( dfil, "\t%g\n", ((double)(QLIN-0.5)) );
+fprintf( dfil, "\t%g\n", ((double)(P->QLIN-0.5)) );
 int i;
-for	( i = 0; i < ( QLOG - 1 ); ++i )
+for	( i = 0; i < ( P->QLOG - 1 ); ++i )
 	{
 	fprintf( dfil, "\t%u\n", ddi[i] ); 
 	fprintf( dfil, "\t%g\n", bnd[i] );
 	}
-fprintf( dfil, "\t%u\n", ddi[QLOG-1] );
+fprintf( dfil, "\t%u\n", ddi[P->QLOG-1] );
 float v; unsigned short u;
 fprintf( dfil, "tests :\n");
 v = 2.0; u = quant( v );
@@ -92,13 +137,14 @@ v = -7000.0; u = quant( v );
 fprintf( dfil, "\t%g -> %u -> %d\n", v, u, dequant[u] );
 fprintf( dfil, " */\n");
 fprintf( dfil, "// definitions a reporter dans audio.h :\n" );
-fprintf( dfil, "// 72000000 / %u = %g Hz\n", target_p * 4, 72000000.0 / ( target_p * 4.0 ) );
-fprintf( dfil, "#define QBIT %d\n", QBIT );
-fprintf( dfil, "#define SAMP_PERIOD  %u\n", target_p * 4 );
-fprintf( dfil, "#define PWM_PERIOD   (SAMP_PERIOD/2)\n" );
-fprintf( dfil, "#define PWM_SILENCE  (PWM_PERIOD/2)\n\n" );
+fprintf( dfil, "// frequence audio = %g Hz\n", ( (double)P->FCK / (double)P->PWMdivider ) / (double)P->Koversamp );
+fprintf( dfil, "#define QBIT %d\n", P->QBIT );
+fprintf( dfil, "#define PWM_SILENCE %d\n", P->target_pk );
+fprintf( dfil, "#define PWM_PERIOD  %d\n", P->PWMdivider );
+fprintf( dfil, "#define SAMP_PERIOD %d\n", P->PWMdivider * P->Koversamp );
+fprintf( dfil, "#define KOVER %d // <-- OVERSAMPLING\n", P->Koversamp );
 fprintf( dfil, "const short dequant[] = {\n" ); 
-for	( i = 0; i < ( ( 1 << QBIT ) - 1 ); ++i )
+for	( i = 0; i < ( ( 1 << P->QBIT ) - 1 ); ++i )
 	{
 	fprintf( dfil, "%d, ", dequant[i] );
 	if	( ( i % 8 ) == 7 )
@@ -126,20 +172,20 @@ int sgn = (diff >= 0.0)?(0):(1);
 float mag = fabs( diff );
 // zone lin : simple round
 unsigned short retval = (unsigned short)round(mag);
-if	( retval >= QLIN )
+if	( retval >= P->QLIN )
 	{			// zone LOG : comparer a chaque bnd[]
 	int i = 0;
 	do	{
 		if	( mag < bnd[i] )
 			break;
 		++i;
-		} while ( i < (QLOG-1) );
-	retval = QLIN + i;
+		} while ( i < (P->QLOG-1) );
+	retval = P->QLIN + i;
 	}
 // on quantifie
-retval &= codemagmask;	// precaution
+retval &= P->codemagmask;	// precaution
 if	( sgn )
-	retval |= codesgnmask;
+	retval |= P->codesgnmask;
 return retval;
 }
 
@@ -160,7 +206,7 @@ return retval;
 int compress2w32( unsigned int qsamp, float * mbuf, unsigned int ** pwbuf )
 {
 // preparation buffer pour packing des codes comprimes
-unsigned qw32 = qsamp * QBIT;
+unsigned qw32 = qsamp * P->QBIT;
 qw32 = ( ( qw32 - 1 ) / 32 ) + 1;
 unsigned int * wbuf = (unsigned int *)malloc( qw32 * sizeof(int) );
 if	( wbuf == NULL )
@@ -171,19 +217,18 @@ unsigned int iw;	// indice du word
 unsigned int pb0;	// position du lsb du code courant dans le word courant
 unsigned int zecode;	// code courant
 unsigned int zew;	// word courant
-// compression precedee de mise a l'echelle target_p
+// compression precedee de mise a l'echelle target_pk
 // packing des codes de QBIT bits dans des mots de 32 bits
 mbuf[0] = 0.0;	// precaution pour eviter offset au depart
-codec_init();
 iw = 0; zew = 0; pb0 = 0;
 for	( ic = 0; ic < qsamp; ++ic )
 	{
-	zecode = (unsigned int)encode( mbuf[ic] * ((float)target_p) );
+	zecode = (unsigned int)encode( mbuf[ic] * ((float)P->target_pk) );
 	#ifdef DUMP_ZECODE
 	printf("%d\n", zecode );
 	#endif
 	zew |= ( zecode << pb0 );
-	pb0 += QBIT;
+	pb0 += P->QBIT;
 	if	( pb0 >= 32 )
 		{			// passer au word suivant
 		if	( iw >= qw32 )	// precaution
@@ -191,7 +236,7 @@ for	( ic = 0; ic < qsamp; ++ic )
 		wbuf[iw++] = zew;
 		pb0 -= 32;
 		if	( pb0 > 0 )	// traiter residu
-			zew = ( zecode >> ( QBIT - pb0 ) );
+			zew = ( zecode >> ( P->QBIT - pb0 ) );
 		else	zew = 0;
 		}
 	}
@@ -216,7 +261,7 @@ float * mbuf = (float *)malloc( qsamp * sizeof(float) );
 if	( mbuf == NULL )
 	return -1;
 // calcul taille buffer w32 pour verif
-unsigned qw32 = qsamp * QBIT;
+unsigned qw32 = qsamp * P->QBIT;
 qw32 = ( ( qw32 - 1 ) / 32 ) + 1;
 if	( qw32 != qwbuf )
 	return -3;
@@ -227,13 +272,12 @@ unsigned int pb0;	// position du lsb du code courant dans le word courant
 unsigned int zecode;	// code courant
 unsigned int zew;	// word courant
 // extraction et decompression des codes de QBIT bits des mots de 32 bits
-codec_init();
 iw = 0; zew = 0; pb0 = 0;
 zew = wbuf[iw++];
 for	( ic = 0; ic < qsamp; ++ic )
 	{
 	zecode = ( zew >> pb0 );
-	pb0 += QBIT;
+	pb0 += P->QBIT;
 	if	( pb0 >= 32 )
 		{			// passer au word suivant
 		if	( iw >= qw32 )	// precaution
@@ -241,10 +285,10 @@ for	( ic = 0; ic < qsamp; ++ic )
 		zew = wbuf[iw++];
 		pb0 -= 32;
 		if	( pb0 > 0 )	// traiter residu
-			zecode |= ( zew << ( QBIT - pb0 ) ); 
+			zecode |= ( zew << ( P->QBIT - pb0 ) ); 
 		}
-	zecode &= ( ( 1 << QBIT ) - 1 );
-	mbuf[ic] = ((float)decode( zecode )) / ((float)target_p);
+	zecode &= ( ( 1 << P->QBIT ) - 1 );
+	mbuf[ic] = ((float)decode( zecode )) / ((float)P->target_pk);
 	}
 if	( ( iw != qw32 ) || ( ic != qsamp ) )
 	{
